790cdb4856ef1e1570c3586ad94c5f9029b89684
wangjiandong <>
2025-06-24 14:43:14 +0800

    --commit=add deps

--- cplusplus-sdk/thirdparty/protobuf-3.21.12/build/CMakeFiles/libprotobuf.dir/src/google/protobuf/io/io_win32.cc.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/libprotobuf.dir/src/google/protobuf/io/io_win32.cc.o: \
-  /Users/apple/wangjiandong/ModelContextProtocl/cplusplus-sdk/thirdparty/tmp/protobuf/src/google/protobuf/io/io_win32.cc
--- cplusplus-sdk/thirdparty/protobuf-3.21.12/build/CMakeFiles/tests.dir/src/google/protobuf/io/io_win32_unittest.cc.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/tests.dir/src/google/protobuf/io/io_win32_unittest.cc.o: \
-  /Users/apple/wangjiandong/ModelContextProtocl/cplusplus-sdk/thirdparty/tmp/protobuf/src/google/protobuf/io/io_win32_unittest.cc
