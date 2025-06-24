a0addd00108334d2782cb29230311a65e77ab4ed
wangjiandong <>
2025-06-24 14:37:26 +0800

    --commit=add deps

---
+++ cplusplus-sdk/thirdparty/protobuf-3.21.12/build/CMakeFiles/protobuf-lite-test-common.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/apple/wangjiandong/ModelContextProtocl/cplusplus-sdk/thirdparty/tmp/protobuf/src/google/protobuf/arena_test_util.cc" "CMakeFiles/protobuf-lite-test-common.dir/src/google/protobuf/arena_test_util.cc.o" "gcc" "CMakeFiles/protobuf-lite-test-common.dir/src/google/protobuf/arena_test_util.cc.o.d"
+  "/Users/apple/wangjiandong/ModelContextProtocl/cplusplus-sdk/thirdparty/tmp/protobuf/src/google/protobuf/map_lite_test_util.cc" "CMakeFiles/protobuf-lite-test-common.dir/src/google/protobuf/map_lite_test_util.cc.o" "gcc" "CMakeFiles/protobuf-lite-test-common.dir/src/google/protobuf/map_lite_test_util.cc.o.d"
+  "/Users/apple/wangjiandong/ModelContextProtocl/cplusplus-sdk/thirdparty/tmp/protobuf/src/google/protobuf/map_lite_unittest.pb.cc" "CMakeFiles/protobuf-lite-test-common.dir/src/google/protobuf/map_lite_unittest.pb.cc.o" "gcc" "CMakeFiles/protobuf-lite-test-common.dir/src/google/protobuf/map_lite_unittest.pb.cc.o.d"
+  "/Users/apple/wangjiandong/ModelContextProtocl/cplusplus-sdk/thirdparty/tmp/protobuf/src/google/protobuf/test_util_lite.cc" "CMakeFiles/protobuf-lite-test-common.dir/src/google/protobuf/test_util_lite.cc.o" "gcc" "CMakeFiles/protobuf-lite-test-common.dir/src/google/protobuf/test_util_lite.cc.o.d"
+  "/Users/apple/wangjiandong/ModelContextProtocl/cplusplus-sdk/thirdparty/tmp/protobuf/src/google/protobuf/unittest_import_lite.pb.cc" "CMakeFiles/protobuf-lite-test-common.dir/src/google/protobuf/unittest_import_lite.pb.cc.o" "gcc" "CMakeFiles/protobuf-lite-test-common.dir/src/google/protobuf/unittest_import_lite.pb.cc.o.d"
+  "/Users/apple/wangjiandong/ModelContextProtocl/cplusplus-sdk/thirdparty/tmp/protobuf/src/google/protobuf/unittest_import_public_lite.pb.cc" "CMakeFiles/protobuf-lite-test-common.dir/src/google/protobuf/unittest_import_public_lite.pb.cc.o" "gcc" "CMakeFiles/protobuf-lite-test-common.dir/src/google/protobuf/unittest_import_public_lite.pb.cc.o.d"
+  "/Users/apple/wangjiandong/ModelContextProtocl/cplusplus-sdk/thirdparty/tmp/protobuf/src/google/protobuf/unittest_lite.pb.cc" "CMakeFiles/protobuf-lite-test-common.dir/src/google/protobuf/unittest_lite.pb.cc.o" "gcc" "CMakeFiles/protobuf-lite-test-common.dir/src/google/protobuf/unittest_lite.pb.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cplusplus-sdk/thirdparty/protobuf-3.21.12/build/CMakeFiles/tests.dir/src/google/protobuf/io/io_win32_unittest.cc.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/tests.dir/src/google/protobuf/io/io_win32_unittest.cc.o: \
+  /Users/apple/wangjiandong/ModelContextProtocl/cplusplus-sdk/thirdparty/tmp/protobuf/src/google/protobuf/io/io_win32_unittest.cc
